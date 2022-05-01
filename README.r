cf5c1e558bcc7ca62e812321a997827b812bf97d
Mohammed Olaiya <mohammedolaiya@gmail.com>
2022-05-01 05:05:53 -0400

    Update README.r

--- README.r
+++ README.r
@@ -20,3 +20,9 @@ my_list
 
 [[3]]
 
+                   mpg cyl  disp  hp drat    wt  qsec vs am gear carb
+Mazda RX4         21.0   6 160.0 110 3.90 2.620 16.46  0  1    4    4
+Mazda RX4 Wag     21.0   6 160.0 110 3.90 2.875 17.02  0  1    4    4
+Datsun 710        22.8   4 108.0  93 3.85 2.320 18.61  1  1    4    1
+Hornet 4 Drive    21.4   6 258.0 110 3.08 3.215 19.44  1  0    3    1
+Hornet Sportabout 18.7   8 360.0 175 3.15 3.440 17.02  0  0    3    2
